d1c3b63ef7995ace6578af5155b0b7f4a528e5f5
rxssula <rxssul.aitkali@gmail.com>
2024-06-13 14:15:46 +0500

    delete events

--- src/global-router.ts
+++ src/global-router.ts
@@ -1,14 +1,10 @@
-import { Router } from 'express';
-import authRouter from './auth/auth-router';
-import eventRouter from './events/event-router';
+import { Router } from "express";
+import authRouter from "./auth/auth-router";
 // other routers can be imported here
 
 const globalRouter = Router();
 
-
 globalRouter.use(authRouter);
-globalRouter.use(eventRouter);
-
 
 // other routers can be added here
 
